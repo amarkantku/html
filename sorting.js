265a0b514441d8f94635ee9ea65556f8a065bd7a
Amarkant Kumar <amar.du2013@gmail.com>
2020-07-28 13:22:34 +0530

    bubble sort added

---
+++ sorting.js
@@ -0,0 +1,25 @@
+let array = [-1,7,8,2,3,99,23,423,33,54,11,21];
+
+function bubbleSort(array) {
+    const len = array.length;
+    let swap = false;
+    let count = 0;
+    for (let i = 1; i < len; i++) {
+        for (let j = 0; j < len-i ; j++) {
+            count++;
+            if (array[j] > array[j+1]) {
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+                swap = true;
+            }
+        }
+        if(swap === false) {
+            break;
+        }
+    }
+    console.log('count', count);
+    return array;
+}
+console.log('Bubble sort => ', bubbleSort(array));
+
